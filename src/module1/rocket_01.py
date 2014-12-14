46379055558b825ca5058df8029a5dd15de7d977
Jerry Kiely <jerry@cowboysmall.com>
2014-12-13 20:50:11 -0500

    cleaned up rocket

--- src/module1/rocket_01.py
+++ src/module1/rocket_01.py
@@ -6,6 +6,24 @@ import matplotlib.pyplot as plt
 from plot import plot_single, plot_multiple, plot_error
 
 
+
+def print_results(results, index1, index2, index3):
+    print
+    print results[0:11, ]
+    print 
+    print ' maximum velocity: %0.2f' % (results[index1, 2])
+    print '             time: %0.2f' % (results[index1, 0])
+    print '         altitude: %0.2f' % (results[index1, 1])
+    print 
+    print '   maximum height: %0.2f' % (results[index2, 1])
+    print '             time: %0.2f' % (results[index2, 0])
+    print 
+    print '   time of impact: %0.2f' % (results[index3, 0])
+    print '         velocity: %0.2f' % (results[index3, 2])
+    print
+
+
+
 def main(argv):
     T     = 40.0
     delta = 0.1
@@ -44,19 +62,7 @@ def main(argv):
     idx_2 = np.argmax(altitude)
     idx_3 = np.where(altitude < 0.0)[0][0]
 
-    print
-    print numerical_result[0:11, ]
-    print 
-    print ' maximum velocity: %0.2f' % (numerical_result[idx_1, 2])
-    print '             time: %0.2f' % (numerical_result[idx_1, 0])
-    print '         altitude: %0.2f' % (numerical_result[idx_1, 1])
-    print 
-    print '   maximum height: %0.2f' % (numerical_result[idx_2, 1])
-    print '             time: %0.2f' % (numerical_result[idx_2, 0])
-    print 
-    print '   time of impact: %0.2f' % (numerical_result[idx_3, 0])
-    print '         velocity: %0.2f' % (numerical_result[idx_3, 2])
-    print
+    print_results(numerical_result, idx_1, idx_2, idx_3)
 
     # plot_single(steps, altitude, ['Altitude'])
     # plot_single(steps, velocity, ['Velocity'])
